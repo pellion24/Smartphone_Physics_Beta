99590d75ed69c303abf5498876c29e4d88a77fa1
Ashvij <ashvijhosdurg@gmail.com>
2026-02-09 14:59:16 -0500

    Initial Commit

---
+++ Smartphone_Physics_Beta/Smartphone_Physics_BetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smartphone_Physics_BetaApp.swift
+//  Smartphone_Physics_Beta
+//
+//  Created by Ashvij Hosdurg on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Smartphone_Physics_BetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
