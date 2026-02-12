2040492bad4842a36ce6d8bc67c77755e4e30acd
Ashvij <ashvijhosdurg@gmail.com>
2026-02-12 14:26:44 -0500

    Worked on connecting views, Lessons view created.

---
+++ Smartphone_Physics_Beta/LessonsData.swift
@@ -0,0 +1,8 @@
+//
+//  LessonsData.swift
+//  Smartphone_Physics_Beta
+//
+//  Created by Ashvij Hosdurg on 2/12/26.
+//
+
+import Foundation
---
+++ Smartphone_Physics_Beta/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Smartphone_Physics_Beta
+//
+//  Created by Ashvij Hosdurg on 2/12/26.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
